c5425e9bf20b992902ec41780d1b190da0afaf99
DevMindNit <164946230+DevMindNit@users.noreply.github.com>
2024-11-09 01:25:00 +0000

    Initial car logic

---
+++ source_code/Car.cpp
@@ -0,0 +1,27 @@
+#include "Car.h"
+
+Car::Car()
+{
+
+}
+
+Car::~Car()
+{
+
+}
+
+void Car::Run()
+{
+    
+}
+
+void Car::setFuelStatus(float fuel)
+{
+    car_fuel_ = fuel;
+}
+
+float Car::getFuelStatus()
+{
+    return car_fuel_;
+}
+
---
+++ source_code/Car.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+class Car
+{
+    public:
+                    Car();
+                    ~Car();
+
+    void            Run();
+
+    void            setFuelStatus(float fuel);
+    float           getFuelStatus() { return car_fuel_; }
+
+    private:
+    float           car_fuel_{1.0f};
+};
